da7e1bfb08e9ee538d8405cfee6d1a5a8dfacfb1
asgor <alexmilesjg@gmail.com>
2023-09-01 16:15:36 +0500

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.SourceDataLine;
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File soundFile = new File("Будь Здоров.mp3");//Звуковой файл
+        AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);//Получаем AudioInputStream
+        int ch = ais.getFormat().getChannels();
+        final float rate = ais.getFormat().getSampleRate();
+        final AudioFormat outFormat = new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
+        final Info info = new Info(SourceDataLine.class, outFormat);
+        final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+
+            if (line != null) {
+                line.open(outFormat);
+                line.start();
+                stream(getAudioInputStream(outFormat, ais), line);
+                line.drain();
+                line.stop();
+            }
+        }
+ public static void stream(AudioInputStream in, SourceDataLine line) throws IOException {
+        final byte[] buffer = new byte[4096];
+        for (int n = 0; n != -1; n = in.read(buffer, 0, buffer.length)) {
+            line.write(buffer, 0, n);
+        }
+    }
+}
+
+
+//        Clip clip = AudioSystem.getClip();//Получаем реализацию интерфейса Clip
+//        clip.open(ais);//Загружаем наш звуковой поток в Clip
+//        clip.setFramePosition(0); //устанавливаем указатель на старт
+//        clip.start(); //Поехали!!!
